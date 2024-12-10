2b446438c1b289d08d43552613ea64661ff5addc
ShadowJu <alikkhol@gmail.com>
2024-12-10 16:49:31 +0200

    feat: complete post api(getbyid,getbysender,update)

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -21,7 +21,56 @@ const getAllPosts = async (req, res) => {
     }
 };
 
-module.exports = {
+const getPostById = async (req, res) => {
+    const postId = req.params.id;
+  
+    try {
+      const post = await Post.findById(postId);
+      if (post) {
+        res.send(post);
+      } else {
+        res.status(404).send("Post was not found");
+      }
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  };
+  
+  const getPostsBySender = async (req, res) => {
+    try {
+      const sender = req.query.sender;
+  
+      const posts = await Post.find({ sender });
+      if (posts.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "No posts found for this sender" });
+      }
+      res.status(200).json(posts);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
+  const updatePost = async (req, res) => {
+    try {
+      const updatedData = req.body;
+      const post = await Post.findByIdAndUpdate(req.params.id, updatedData, {
+        new: true,
+      });
+      if (!post) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+      res.status(200).json(post);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
+  module.exports = {
     createPost,
     getAllPosts,
-};
+    getPostById,
+    getPostsBySender,
+    updatePost,
+  };
