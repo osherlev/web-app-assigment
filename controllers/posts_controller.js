302e5342057d947db1a89e568d3001b2770f080c
osherlev <osherlev317@gmail.com>
2024-12-10 16:22:36 +0200

    fix: remove filter

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,27 +1,26 @@
 const Post = require("../models/posts_model");
 
 const createPost = async (req, res) => {
-  const postBody = req.body;
-  try {
-    const post = await Post.create(postBody);
-    res.status(201).send(post);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
+    const postBody = req.body;
+    try {
+        const post = await Post.create(postBody);
+        res.status(201).send(post);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 };
 
 const getAllPosts = async (req, res) => {
-  const filter = req.query.sender;
-  let posts;
-  try {
-    posts = filter ? await Post.find({ sender: filter }) : await Post.find();
-    res.send(posts);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
+    let posts;
+    try {
+        posts = await Post.find();
+        res.send(posts);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 };
 
 module.exports = {
-  createPost,
-  getAllPosts,
+    createPost,
+    getAllPosts,
 };
